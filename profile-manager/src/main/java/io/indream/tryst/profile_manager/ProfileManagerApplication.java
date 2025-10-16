f88504645843a8e1cbe2e3387a10f557523be46b
akshayg <akshay.guruprasad2023@gmail.com>
2025-10-17 00:05:15 +0530

    init commit

---
+++ profile-manager/src/main/java/io/indream/tryst/profile_manager/ProfileManagerApplication.java
@@ -0,0 +1,13 @@
+package io.indream.tryst.profile_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileManagerApplication.class, args);
+	}
+
+}
---
+++ profile-manager/src/test/java/io/indream/tryst/profile_manager/ProfileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.indream.tryst.profile_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
